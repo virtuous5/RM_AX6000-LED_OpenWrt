940cc38bab810ff312e06b976d5d06244aea091a
virtuous5 <62429535+virtuous5@users.noreply.github.com>
2024-03-16 03:54:57 +0800

    Update diy-part1.sh

--- hanwckf/Redmi-AX6000/diy-part1.sh
+++ hanwckf/Redmi-AX6000/diy-part1.sh
@@ -30,7 +30,7 @@ sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.d
 ./scripts/feeds install -f -p mosdns mosdns luci-app-mosdns
 find ./ -name v2ray-geodata | xargs rm -rf
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-svn ex# port https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-vlmcsd package/luci-app-vlmcsd
-svn ex# port https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/vlmcsd  package/vlmcsd
+# svn ex port https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-vlmcsd package/luci-app-vlmcsd
+# svn ex port https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/vlmcsd  package/vlmcsd
 sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
 ./scripts/feeds update -i && ./scripts/feeds install -a
